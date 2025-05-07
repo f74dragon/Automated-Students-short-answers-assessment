9c8ea122c51e9e58559db850ad3c2a8b90679597
f74dragon <75919123+f74dragon@users.noreply.github.com>
2025-05-06 22:52:05 -0400

    Create rebuild.sh

---
+++ LINUXDOCKER/rebuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker stop my-ubuntu-container 2>/dev/null && docker rm my-ubuntu-container 2>/dev/null
+
+docker build -t my-ubuntu-dev .
+
+docker run -it \
+  -p 3000:3000 \
+  -p 8001:8001 \
+  -p 5432:5432 \
+  -p 11434:11434 \
+  --name my-ubuntu-container \
+  my-ubuntu-dev
